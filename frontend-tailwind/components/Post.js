d9e76876b381ec8de339c34dea9ef69200fc5abf
grignaffini <grignaffinipaolo@gmail.com>
2022-08-10 17:11:47 +0200

    fixed build

--- frontend-tailwind/components/Post.js
+++ frontend-tailwind/components/Post.js
@@ -16,18 +16,6 @@ import { NewtonsCradle } from '@uiball/loaders'
 
 function Post({ post }) {
 
-    if (!post) {
-        return (
-            <div className="flex w-full h-screen items-center justify-center p-10 text-3-xl">
-                <NewtonsCradle
-                    size={50}
-                    speed={1.4}
-                    color="black"
-                />
-            </div>
-        )
-    }
-
     const { data: session } = useSession()
     const { data, loading } = useQuery(GET_COMMENTS_BY_POST_ID, {
         variables: { id: post?.id }
@@ -110,94 +98,105 @@ function Post({ post }) {
 
     }, [voteData])
 
-    return (
-
-        <div className={`bg-base-100 my-7 max-w-3xl border rounded-t-2xl rounded-b-2xl shadow-sm cursor-pointer`}>
-            <Link href={`/post/${post.id}`}>
-                <div>
-                    <div className="flex items-center p-5">
-                        <img src={post?.user?.profile_pic} className="rounded-full h-12 object-contain border p-1 mr-3" />
-                        <div className="flex-col flex-1">
-                            <p className="font-bold">{post?.user?.username}</p>
-                            <Link href={`/category/${post?.category?.name}`}>
-                                <p className="text-sm cursor-pointer hover:text-info hover:underline">p/{post?.category?.name}</p>
-                            </Link>
-                        </div>
-                        <TimeAgo className="text-sm" date={post?.created_at} />
-                    </div>
-
-                    <div className="p-7">
-                        <h1 className="text-base-content text-lg font-semibold">{post?.title}</h1>
-                        <p className="m-5">{post?.body}</p>
-                    </div>
-
-                    <img src={post?.url} className="object-cover w-full" />
-
+    if (!post) {
+        return (
+            <div className="flex w-full h-screen items-center justify-center p-10 text-3-xl">
+                <NewtonsCradle
+                    size={50}
+                    speed={1.4}
+                    color="black"
+                />
+            </div>
+        )
+    } else {
+        return (
+            <div className={`bg-base-100 my-7 max-w-3xl border rounded-t-2xl rounded-b-2xl shadow-sm cursor-pointer`}>
+                <Link href={`/post/${post.id}`}>
                     <div>
-                        <div className="p-5 truncate">
-                            {/* {likes.length > 0 && (
+                        <div className="flex items-center p-5">
+                            <img src={post?.user?.profile_pic} className="rounded-full h-12 object-contain border p-1 mr-3" />
+                            <div className="flex-col flex-1">
+                                <p className="font-bold">{post?.user?.username}</p>
+                                <Link href={`/category/${post?.category?.name}`}>
+                                    <p className="text-sm cursor-pointer hover:text-info hover:underline">p/{post?.category?.name}</p>
+                                </Link>
+                            </div>
+                            <TimeAgo className="text-sm" date={post?.created_at} />
+                        </div>
+
+                        <div className="p-7">
+                            <h1 className="text-base-content text-lg font-semibold">{post?.title}</h1>
+                            <p className="m-5">{post?.body}</p>
+                        </div>
+
+                        <img src={post?.url} className="object-cover w-full" />
+
+                        <div>
+                            <div className="p-5 truncate">
+                                {/* {likes.length > 0 && (
                         <p className="font-bold mb-1">{likes.length} likes</p>
                     )} */}
-                            {
-                                comments?.length > 0 && (
-                                    <p className="font-semibold text-sm mb-1">{comments?.length} comments</p>
-                                )
-                            }
+                                {
+                                    comments?.length > 0 && (
+                                        <p className="font-semibold text-sm mb-1">{comments?.length} comments</p>
+                                    )
+                                }
+                            </div>
                         </div>
+
+                        {comments.length > 0 && (
+                            <div className="ml-6 h-20 overflow-y-scroll scrollbar-hide scrollbar-thumb-black scrollbar-thin">
+                                {comments.map(comment => (
+                                    <div key={comment.id} className="flex items-center space-x-2 mb-3">
+                                        <img className="h-7 rounded-full" src={comment?.user?.profile_pic} />
+                                        <p className="text-sm flex-1">
+                                            <span className="font-bold">{comment?.user?.username}</span>
+                                            {" "}{comment.body}
+                                        </p>
+                                        <TimeAgo className="text-sm px-4" date={comment?.created_at} />
+                                    </div>
+                                ))}
+                            </div>
+                        )}
                     </div>
+                </Link>
 
-                    {comments.length > 0 && (
-                        <div className="ml-6 h-20 overflow-y-scroll scrollbar-hide scrollbar-thumb-black scrollbar-thin">
-                            {comments.map(comment => (
-                                <div key={comment.id} className="flex items-center space-x-2 mb-3">
-                                    <img className="h-7 rounded-full" src={comment?.user?.profile_pic} />
-                                    <p className="text-sm flex-1">
-                                        <span className="font-bold">{comment?.user?.username}</span>
-                                        {" "}{comment.body}
-                                    </p>
-                                    <TimeAgo className="text-sm px-4" date={comment?.created_at} />
-                                </div>
-                            ))}
+                {
+                    session &&
+                    <>
+                        <div className="flex justify-between items-center bg-base-100 text-base-content border-t">
+                            <div className={`flex space-x-1 items-center hover:bg-base-200 flex-grow justify-center p-2 rounded-xl cursor-pointer hover:text-success ${vote && 'text-success'}`}
+                                onClick={() => upVote(true)}>
+                                {vote ? <ThumbUpIconFilled className="h-4" /> : <ThumbUpIcon className="h-4" />}
+                                <p className="text-xs sm:text-base">Upvote</p>
+                            </div>
+                            <div className={`flex space-x-1 items-center hover:bg-base-200 flex-grow justify-center p-2 rounded-xl cursor-pointer hover:text-error ${vote === false && 'text-error'}`}
+                                onClick={() => upVote(false)} >
+                                {vote === false ? <ThumbDownIconFilled className="h-4" /> : <ThumbDownIcon className="h-4" />}
+                                <p className="text-xs sm:text-base">Downvote</p>
+                            </div>
+                            <div className="flex space-x-1 items-center hover:bg-base-200 flex-grow justify-center p-2 rounded-xl cursor-pointer ">
+                                <ShareIcon className="h-4" />
+                                <p className="text-xs sm:text-base">Share</p>
+                            </div>
                         </div>
-                    )}
-                </div>
-            </Link>
 
-            {
-                session &&
-                <>
-                    <div className="flex justify-between items-center bg-base-100 text-base-content border-t">
-                        <div className={`flex space-x-1 items-center hover:bg-base-200 flex-grow justify-center p-2 rounded-xl cursor-pointer hover:text-success ${vote && 'text-success'}`}
-                            onClick={() => upVote(true)}>
-                            {vote ? <ThumbUpIconFilled className="h-4" /> : <ThumbUpIcon className="h-4" />}
-                            <p className="text-xs sm:text-base">Upvote</p>
-                        </div>
-                        <div className={`flex space-x-1 items-center hover:bg-base-200 flex-grow justify-center p-2 rounded-xl cursor-pointer hover:text-error ${vote === false && 'text-error'}`}
-                            onClick={() => upVote(false)} >
-                            {vote === false ? <ThumbDownIconFilled className="h-4" /> : <ThumbDownIcon className="h-4" />}
-                            <p className="text-xs sm:text-base">Downvote</p>
-                        </div>
-                        <div className="flex space-x-1 items-center hover:bg-base-200 flex-grow justify-center p-2 rounded-xl cursor-pointer ">
-                            <ShareIcon className="h-4" />
-                            <p className="text-xs sm:text-base">Share</p>
-                        </div>
-                    </div>
-
-                    <form className="flex items-center p-4 border-t">
-                        <EmojiHappyIcon className="h-7" />
-                        <input
-                            type="text"
-                            value={comment}
-                            onChange={e => setComment(e.target.value)}
-                            placeholder="Add a comment..."
-                            className="border-none bg-base-100 flex-1 focus:ring-0 outline-none"
-                        />
-                        <button type="submit" onClick={sendComment} disabled={!comment.trim()} className="font-semibold text-primary hover:text-primary-focus cursor-pointer">Comment</button>
-                    </form>
-                </>
-            }
-        </div >
-    )
+                        <form className="flex items-center p-4 border-t">
+                            <EmojiHappyIcon className="h-7" />
+                            <input
+                                type="text"
+                                value={comment}
+                                onChange={e => setComment(e.target.value)}
+                                placeholder="Add a comment..."
+                                className="border-none bg-base-100 flex-1 focus:ring-0 outline-none"
+                            />
+                            <button type="submit" onClick={sendComment} disabled={!comment.trim()} className="font-semibold text-primary hover:text-primary-focus cursor-pointer">Comment</button>
+                        </form>
+                    </>
+                }
+            </div >
+        )
+    }
 }
 
 export default Post
