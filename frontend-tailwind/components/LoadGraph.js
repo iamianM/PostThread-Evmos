cbf228615550155a6f2a93f4a3c2933c759189a9
grignaffini <grignaffinipaolo@gmail.com>
2022-08-19 15:02:36 +0200

    implemented airdrop

--- frontend-tailwind/components/LoadGraph.js
+++ frontend-tailwind/components/LoadGraph.js
@@ -21,32 +21,30 @@ export const LoadGraph = () => {
         }
     })
 
-    const isBrowser = () => typeof window !== "undefined";
-    if (isBrowser) {
+
+    useEffect(() => {
         const loadGraph = useLoadGraph();
-        useEffect(() => {
-            const graph = new Graph();
-            graph.addNode("John", { x: 0, y: 10, size: 5, label: "John", color: "blue" });
+        const graph = new Graph();
+        graph.addNode("John", { x: 0, y: 10, size: 5, label: "John", color: "blue" });
 
-            followers?.getFollowersByUser_id?.map(follow => {
-                graph.addNode(follow?.follower?.username, { x: 0 + xOffset, y: 10 + yOffset, size: 5, label: follow?.follower?.username, color: "green" })
-                graph.addEdge(follow?.follower?.username, "John")
-                setXOffset(prevXOffset => prevXOffset + 1)
-                setYOffset(prevYOffset => prevYOffset + 1)
-            })
+        followers?.getFollowersByUser_id?.map(follow => {
+            graph.addNode(follow?.follower?.username, { x: 0 + xOffset, y: 10 + yOffset, size: 5, label: follow?.follower?.username, color: "green" })
+            graph.addEdge(follow?.follower?.username, "John")
+            setXOffset(prevXOffset => prevXOffset + 1)
+            setYOffset(prevYOffset => prevYOffset + 1)
+        })
 
-            following?.getFollowingsByUser_id?.map((follow, index) => {
-                graph.addNode(follow?.following?.username, { x: 10 + index, y: 10 - 5 * index, size: 5, label: follow?.following?.username, color: "red" })
-                graph.addEdge("John", follow?.following?.username)
-                setXOffset(prevXOffset => prevXOffset + 1)
-                setYOffset(prevYOffset => prevYOffset + 1)
-            })
+        following?.getFollowingsByUser_id?.map((follow, index) => {
+            graph.addNode(follow?.following?.username, { x: 10 + index, y: 10 - 5 * index, size: 5, label: follow?.following?.username, color: "red" })
+            graph.addEdge("John", follow?.following?.username)
+            setXOffset(prevXOffset => prevXOffset + 1)
+            setYOffset(prevYOffset => prevYOffset + 1)
+        })
 
-            loadGraph(graph);
-        }, [loadGraph, followers, following]);
+        loadGraph(graph);
+    }, [loadGraph, followers, following]);
 
-        return loadGraph;
-    } else return <p>NOT AVAILABLE</p>;
+    return loadGraph;
 };
 
 export default LoadGraph;
